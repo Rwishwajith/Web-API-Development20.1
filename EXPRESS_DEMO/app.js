7a41931937026e6ba4ec042298e9c328d43974e1
Ruchira Wishwajith <52341241+Rwishwajith@users.noreply.github.com>
2021-07-21 20:51:01 +0530

    Expree_demo_routers
    Expree_demo_routers

--- EXPRESS_DEMO/app.js
+++ EXPRESS_DEMO/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const authenticationHandler = require ("./Middlewares/authentication"); //link the authentication
 const loggerHandler = require("./Middlewares/logger");
+const bears = require("./routers/bears");
 const app = express();
 
 app.use(express.json()); //Parse JSON objects in req
 app.use(authenticationHandler);
 app.use(loggerhandler);
+app.use('api/bears', bears);
 
 //middleware creation
 app.use((req, res, next ) => {
@@ -17,30 +19,10 @@ app.use((req, res, next ) => {
     console.log("Middleware 2 is executing.....");
     res.send();
 });
-let beararry = [
-    {id:1,name = 'Black bear', type =  'American', place = 'North America'},
-    {id:2,name = 'Polar bear', type =  'White', place = 'North Pole'},
-    {id:3,name = 'Asian bear', type =  'Back bear', place = 'Indian'}
 
-]
 app.get("/", (req,res) => {
 res.send("Hello World");
 
-});
-
-app.get("/api/bears", (req, res) => {
-    let bears =['Black bear', 'polar bear', 'American Bear'];
-    res.send(bears);
-});
-
-app.get("/api/bears/:bearId", (req, res) => {
-    let bearId = req.params.bearId;
-    let bear = beararry.find(b => b.id == bearId);
-    if (!bear)
-    {
-        return res.status(404).send("Given ID Does not exist");
-    }
-    res.send(bear);
 });
 
 app.listen(3000);
